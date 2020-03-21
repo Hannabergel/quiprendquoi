4b59cdbf42791941f3726d628a3726586bb81658
Hannabergel <hanna.bergel99@gmail.com>
2020-03-21 21:57:44 +0100

    6, 7, 8: template pug et passage de variable

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+const dotenv = require('dotenv').config();
 
 app.get('/', function(req, res) {
-  res.send('Bonjour');
+    res.render('index', { title: 'Qui prend quoi ?' });
 });
 
-app.listen(port, () => console.log(`Front app listening on port ${port}!`));
+app.set('view engine', 'pug');
+
+
+app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
